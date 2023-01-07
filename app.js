70e3f89eb5503ac2ae0706b6da37f4d3c504a286
Cole Gebhard <gebhardcole@gmail.com>
2023-01-06 18:04:57 -0700

    added single page view

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const app = express();
 const PORT = 1337;
 
 app.use(express.static('public'))
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => {
 
-  const postList = postBank.list()  ;
-  const html = 
-  `<!DOCTYPE html>
+  const postList = postBank.list();
+  const html =
+    `<!DOCTYPE html>
   <html>
   <head>
     <title>Wizard News</title>
@@ -25,24 +26,55 @@ app.get("/", (req, res) => {
         <div class='news-item'>
           <p>
             <span class="news-position">${post.id}. ▲</span>
-            ${post.title}
+            <a href="/posts/${post.id}">${post.title}</a>
             <small>(by ${post.name})</small>
           </p>
           <small class="news-info">
             ${post.upvotes} upvotes | ${post.date}
           </small>
         </div>`
-      ).join('')}
+    ).join('')}
     </div>
   </body>
 </html>`
 
-      res.send(html);
+  res.send(html);
 
 });
 
-app.use(morgan('dev'));
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html =
+    `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class='news-item'>
+      <p>
+        <span class="news-position">${post.id}. ▲</span>
+        ${post.title}
+        <small>(by ${post.name})</small>
+      </p>
+      <p>${post.content}</p>
+      <small class="news-info">
+        ${post.upvotes} upvotes | ${post.date}
+      </small>
+    </div>
+    </div >
+  </body >
+</html > `
+
+res.send(html)
+})
+
+
 
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log(`App listening in port ${ PORT } `);
 });
